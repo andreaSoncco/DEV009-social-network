0a8d40fb288d045b6eb14aaed3c7c999406f4ac6
Astrid Andrea Bolaños Garcia <l.n.andreagarcia@hotmail.com>
2023-07-27 20:54:04 -0600

    Estilos y codigo de pagina home actualidos

--- src/components/home.js
+++ src/components/home.js
@@ -1,14 +1,25 @@
 function home(navigateTo) {
-  const section = document.createElement('section');
-  section.id = 'Home';
-  const title = document.createElement('h2');
-  title.textContent = 'Bienvenid@ a DIY Lovers ';
+  const divHome = document.createElement('div');
+  divHome.id = 'Home';
+  //Logo
+  const logo = document.createElement('img');
+  logo.id = 'logoBienvenida';
+  logo.src = 'img/Logo.png';
+  // section caja de bienvenida
+  const sectionBienvenida = document.createElement('section');
+  sectionBienvenida.id = 'sectionBienvenida';
+  const title = document.createElement('h1');
+  title.id = 'titleBienvenida';
+  title.innerText = 'Emplea el arte para expresar tus sentimientos y forma parte de la mejor comunidad de artistas';
+  // botón de entrar
   const buttonLogin = document.createElement('button');
-  buttonLogin.innerText = 'Login';
+  buttonLogin.id = 'buttonLogin';
+  buttonLogin.innerText = 'Entrar';
   buttonLogin.addEventListener('click', () => {
     navigateTo('/login');
   });
-  section.append(title, buttonLogin);
-  return section;
+  divHome.append(logo, sectionBienvenida);
+  sectionBienvenida.append(title, buttonLogin);
+  return divHome;
 }
 export default home;
