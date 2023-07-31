b54754dbd80fcf570179e4fb71524072663af394
unknown <lalujoya@gmail.com>
2023-07-31 18:36:04 -0500

    se crea main y form en login.js se da estilo pag login y se agrega img btn google

--- src/components/login.js
+++ src/components/login.js
@@ -1,41 +1,69 @@
 function login(navigateTo) {
-  const firstSection = document.createElement('section');
-  firstSection.id = 'first';
-  const section = document.createElement('section');
-  section.id = 'login';
+ 
+  const Login = document.createElement("main");
+  Login.classList.add("login");
+  const formLogin = document.createElement("form");
+  formLogin.classList.add("formLogin");
+  const logintitle = document.createElement("h2");
+  logintitle.innerHTML = `Inicia Sesión`;
   const img = document.createElement('img');
-  img.id = 'logoLogin';
-  img.src = 'img/Logo.png';
-  const title = document.createElement('h2');
-  title.id = 'logintitle';
-  title.textContent = 'Login ';
+  img.setAttribute("id", "logoLogin")
+  img.setAttribute("src",'img/Logo.png')
+
+  const inputEmail = document.createElement("input");
+  inputEmail.setAttribute("type", "email");
+  inputEmail.setAttribute("id", "email");
+  inputEmail.setAttribute("placeholder", "Email");
+  inputEmail.setAttribute("size", "25");
+  inputEmail.setAttribute("maxlength", "40");
+  inputEmail.setAttribute("required", "");
+
+  const inputPassword = document.createElement("input");
+  inputPassword.setAttribute("type", "password");
+  inputPassword.setAttribute("id", "password");
+  inputPassword.setAttribute("placeholder", "Password");
+  inputPassword.setAttribute("minlength", "6");
+  inputPassword.setAttribute("maxlength", "12");
+  inputPassword.setAttribute("required", "");
+
+  const buttonLogin = document.createElement("button");
+  buttonLogin.setAttribute("type", "submit");
+  buttonLogin.textContent = 'Login';
+  buttonLogin.addEventListener('click', () => {
+    navigateTo('/wall');
+  })
+  
   const buttonHome = document.createElement('button');
-  buttonHome.textContent = 'return to home';
+  buttonHome.textContent = 'Back Home';
   buttonHome.addEventListener('click', () => {
     navigateTo('/');
   });
-  const inputEmail = document.createElement('input');
-  inputEmail.id = 'email';
-  inputEmail.placeholder = 'email';
-  const inputPassword = document.createElement('input');
-  inputPassword.placeholder = 'Password';
-  const buttonLogin = document.createElement('button');
-  buttonLogin.textContent = 'Login';
 
-  // Cambio newAccount
-  buttonLogin.addEventListener('click', () => {
-    navigateTo('/wall');
-  });
-
-  const buttonNewAccount = document.createElement('button');
-  buttonNewAccount.textContent = 'Crear Cuenta';
-  buttonNewAccount.addEventListener('click', () => {
+  const btnCreateAccount = document.createElement('button');
+  btnCreateAccount.textContent = 'Crear Cuenta';
+  btnCreateAccount.addEventListener('click', () => {
     navigateTo('/newAccount');
   });
 
-  firstSection.appendChild(section);
-  section.append(img, title, inputEmail, inputPassword, buttonLogin, buttonNewAccount, buttonHome);
+  const btnGoogle = document.createElement('img');
+  btnGoogle.setAttribute("id", "btnGoogle");
+  btnGoogle.setAttribute("src",'img/google.png');
+  btnGoogle.addEventListener('click', () => {
+    navigateTo('/google');
+})
 
-  return firstSection;
+  const forgPassw = document.createElement("p");
+  forgPassw.innerHTML = `<a href="forgotPass.js">¿Olvidaste la contraseña?</a>`;
+
+  const createAccount = document.createElement("p");
+  createAccount.innerHTML = `¿No tienes cuenta?`;
+
+  const option = document.createElement("p");
+  option.innerHTML = 'o';
+
+ Login.appendChild(formLogin);
+ formLogin.append(img,logintitle, inputEmail, inputPassword, buttonLogin, forgPassw, createAccount, btnCreateAccount, option, btnGoogle, buttonHome);
+  return Login;
 }
 export default login;
+
