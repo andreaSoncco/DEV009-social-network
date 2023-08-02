efec9ad0dd62121c122bbad6eaaabe05137f13c5
unknown <lalujoya@gmail.com>
2023-08-02 10:44:41 -0500

    se crea funcion para iniciar sesion  con google se crea banner para muro se trae logo orig de google

--- src/components/wall.js
+++ src/components/wall.js
@@ -1,18 +1,20 @@
 function wall(navigateTo) {
-  const divWall = document.createElement('div');
+  const divWall = document.createElement('body');
   divWall.id = 'wall';
 
   // section General
   const section = document.createElement('section');
-  section.id = 'sectionWall';
+  section.id = 'sectionWall'; 
+  const header = document.createElement('header');
+  header.id = 'headerWall'; 
   const buttonHome = document.createElement('button');
   buttonHome.textContent = 'Volver atrás';
   buttonHome.addEventListener('click', () => {
     navigateTo('/');
   });
-
+  
   divWall.append(section);
-  section.append(buttonHome);
+  section.append(header, buttonHome);
 
   return divWall;
 }
--- src/lib/initializeFirebase.js
+++ src/lib/initializeFirebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from import { initializeApp } from 'firebase/app';the SDKs you need
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider } from "firebase/auth";
 import { app } from "./credentialsFirebase.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -25,4 +25,33 @@ export const registrarUsuario = (email, password) => {
     const errorMessage = error.message;
     // ..
   });
+};
+
+
+const provider = new GoogleAuthProvider();
+
+export const registerGoogle = (callback) => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      const nameUser = user.displayName;
+      userDataGoogle();
+      callback(true);
+
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+      callback(false);
+    });
+
 };
