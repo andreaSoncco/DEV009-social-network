37fea6deb2f0339f337199e7d83111587564d9e2
andreaSoncco <andrea.soncco.c@gmail.com>
2023-08-11 12:34:52 -0500

    maquetación del encabezado del muro

--- src/components/wall.js
+++ src/components/wall.js
@@ -5,14 +5,25 @@ function wall(navigateTo) {
   // section General
   const section = document.createElement('section');
   section.id = 'sectionWall'; 
+
   const header = document.createElement('header');
   header.id = 'headerWall'; 
+  
+  const logoWall = document.createElement('img');
+  logoWall.id = 'logoWall';
+  logoWall.src = 'img/logo.png';
+
+  const toggleWall = document.createElement('i');
+  toggleWall.id = "toggleWall";
+  toggleWall.classList = "fa-solid fa-bars";
+
   const buttonHome = document.createElement('button');
   buttonHome.textContent = 'Volver atrás';
   buttonHome.addEventListener('click', () => {
     navigateTo('/');
   });
   
+  header.append(logoWall, toggleWall);
   divWall.append(section);
   section.append(header, buttonHome);
 
