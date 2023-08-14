e6f2173fb01d49811398cca3cd06825c5450a06c
Astrid Andrea Bolaños Garcia <l.n.andreagarcia@hotmail.com>
2023-08-14 10:59:22 -0600

    ajustes css y logo del wall, actualizacion de la imagenes en la carpeta img

--- src/components/wall.js
+++ src/components/wall.js
@@ -1,20 +1,27 @@
 function wall(navigateTo) {
-  const divWall = document.createElement('body');
-  divWall.id = 'wall';
+  const divWall = document.createElement('div');
+  divWall.id = 'timeLine';
 
-  // section General
-  const section = document.createElement('section');
-  section.id = 'sectionWall'; 
-  const header = document.createElement('header');
-  header.id = 'headerWall'; 
+  // section banner
+  const sectionBanner = document.createElement('section');
+  sectionBanner.id = 'banner'; 
+  const header = document.createElement('img');
+  header.id = 'logoWall';
+  header.src = 'img/logo.png';
+  const user = document.createElement('select');
+  
+  //section wall updates
+  const sectionTimeLine = document.createElement('section');
+  sectionTimeLine.id = 'sectionTimeLine';
   const buttonHome = document.createElement('button');
   buttonHome.textContent = 'Volver atrás';
   buttonHome.addEventListener('click', () => {
     navigateTo('/');
   });
   
-  divWall.append(section);
-  section.append(header, buttonHome);
+  divWall.append(sectionBanner, sectionTimeLine);
+  sectionBanner.append(header)
+  sectionTimeLine.append( buttonHome);
 
   return divWall;
 }
