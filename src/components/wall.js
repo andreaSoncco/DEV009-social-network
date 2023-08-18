28fefd76ac9d954152413aeb0f48b944d52a1bfd
Astrid Andrea Bolaños Garcia <l.n.andreagarcia@hotmail.com>
2023-08-18 01:55:19 -0600

    Se agrega funcion que muestra los comentarios desde un inicio en el Wall. Se esta trabajando en la funcion del conteo de Likes. Estructurar bien el css, el area de los posts

--- src/components/wall.js
+++ src/components/wall.js
@@ -1,12 +1,11 @@
 import { async } from "regenerator-runtime";
-import { createPost, getPostsByUser, auth, getAllPosts } from "../lib/initializeFirebase.js";
+import { createPost, getPostsByUser, auth, getAllPosts, getPostsOrderByDateTime, getPostsbyUid } from "../lib/initializeFirebase.js";
 
 
 function wall(navigateTo) {
   const sectionWall = document.createElement('section');
   sectionWall.id = 'timeLine';
 
-  
   /* section banner
   const sectionBanner = document.createElement('section');
   sectionBanner.id = 'banner'; 
@@ -58,9 +57,20 @@ inputNewPost.placeholder = "¿Qué te inspiró hoy?";
 const buttonPublishNewPost = document.createElement('button');
 buttonPublishNewPost.id = 'buttonPublish';
 buttonPublishNewPost.innerText = 'Publicar';
-buttonPublishNewPost.addEventListener('click', () =>{
 
-})
+// con esta instucción, publica y de inmediato se muestra en el muro la publicación
+buttonPublishNewPost.addEventListener('click', async () =>{
+// crea un nuevo post
+  createPost(inputNewPost.value);
+  //obtiene todos los posts publicados de todos los usuarios
+  const postJSON = await getAllPosts();
+  //obtiene el arreglo de posts
+  const allPosts = postJSON.posts;
+  // muestra todos los posts 
+  postList(allPosts);
+
+});
+
 divNewPost.append(inputNewPost, buttonPublishNewPost);
 
 /*-----------------------------------------Cajas para despligue de publicaciones------------------------ */
@@ -68,7 +78,8 @@ const divAllPosts = document.createElement('div');
 divAllPosts.id = "allPosts";
 const divPost = document.createElement('div');
 divPost.id = "divPost";
-divPost.innerHTML = `
+
+/*divPost.innerHTML = `
 <div id="userPost"><p>aqui va el usuario</p></div>
 <div id="textPost"><p>aqui va el texto de la publicacion</p></div>
 <div id="postInteractions">
@@ -76,11 +87,11 @@ divPost.innerHTML = `
 <button id="readComments>Leer comentarios</button>
 <button id="postLikes">Likes<button>
 </div>
-`
+`;*/
 divAllPosts.appendChild(divPost);
 
 /*-----------------------------------BOTONES------------------------------------ */
-  const buttonHome = document.createElement('button');
+ /* const buttonHome = document.createElement('button');
   buttonHome.textContent = 'Volver atrás';
   buttonHome.addEventListener('click', () => {
     navigateTo('/');
@@ -101,42 +112,97 @@ divAllPosts.appendChild(divPost);
     const postJSON = await getAllPosts();
     const allPosts = postJSON.posts;
     postList(allPosts);
-    console.log("esto deberia ser lo ultimo en consola");
+    //console.log("esto deberia ser lo ultimo en consola");
     console.log(postJSON);
     //getAllPosts();
    
-  });
+  });*/
+  
   const postList = (list) => {
     // let countPokemon = 0;
-  
+    //console.log(list);
+    
+    divAllPosts.innerHTML = '';
+    console.log(auth);
     list.forEach(objPost => {
       const content = document.createElement("div");
+      content.classList.add("art-wrap");
       //content.classList.add("content-principal");
-      content.innerHTML = `
+    /*  content.innerHTML = `
            <div >
            <p >${objPost.post}</p>
-           </div>`;
-      /*content.addEventListener('click', () => {
-        const viewModal = showModal(pokemon);
-        viewModal.classList.add('modal--show');
-        content.appendChild(contentModal);
-      });*/
-  
-      //countPokemon += 1;
-      sectionWall.appendChild(content);
+           </div>`;*/
+
+         
+      content.innerHTML = `  <div class="art-header" id = ${objPost.idPost}>
+      <img src="https://cdn-icons-png.flaticon.com/512/17/17603.png" alt="" class="avator">
+      <div class="art-header-info">
+        ${objPost.userDisplayName} <span>${objPost.userEmail}</span><span>.
+  </span>
+        <p>${objPost.post}</p>
+        
+      </div>
+      
+    </div>
+
+    <div class="art-info-counts">
+      
+      <div class="comments">
+        
+        <svg class="feather feather-message-circle sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg>
+        <div class="comment-count">${objPost.comments}</div>
+      </div>
+
+      
+      <div class="likes">
+        <svg  class="feather feather-heart sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+        <div class="likes-count">
+          ${objPost.likeCount}
+        </div>
+      </div>
+
+    </div>`;
+    
+    content.addEventListener('click', () => {
+      alert("entro a like" + objPost.idPost);
+      getPostsbyUid(objPost.idPost);
+      
+    });
+      divAllPosts.appendChild(content);
   
     });
   };
+
+
+
+
+function executeGetPostsbyUid(postId) {
+alert(postId);
+}
+
   
+  //código que carga todos los posts al inicio, se están reultizando métodos 
+  const loadAllPostStart = async () =>
+  {
+    //const postJSON = await getAllPosts();
+    const postJSON = await getPostsOrderByDateTime();
+    const allPosts = postJSON.posts;
+    postList(allPosts);
+    console.log(postJSON);
+  }
+  //esta función hace que se carguen todos los posts al inicio
+  loadAllPostStart();
+
  /* divWall.append(sectionBanner, sectionTimeLine);
   sectionBanner.append(header)
   sectionTimeLine.append( buttonHome);*/
   sectionWall.append(header, divNewPost, sectionBotonesDePrueba, divAllPosts);
   header.append(logoWall, menuSelect);
-  sectionBotonesDePrueba.append(buttonHome, addPostOnFireBase, getPostFromFireBase, imagenTemporal);
+  //sectionBotonesDePrueba.append(buttonHome, addPostOnFireBase, getPostFromFireBase, imagenTemporal);
   
   
 
   return sectionWall;
 }
 export default wall;
+
